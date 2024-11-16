33a611866f48f92764ae34cc20ac09af6423a194
Mirza Hilmi <dev@mrz.my.id>
2024-11-16 15:23:36 +0700

    refactor: remove unused comments

--- user-service/internal/config/config.go
+++ user-service/internal/config/config.go
@@ -19,29 +19,11 @@ type postgreSQL struct {
 }
 
 type logging struct {
-	Level string
-	// Development puts the logger in development mode, which changes the
-	// behavior of DPanicLevel and takes stacktraces more liberally.
-	Development bool
-	// DisableCaller stops annotating logs with the calling function's file
-	// name and line number. By default, all logs are annotated.
-	DisableCaller bool
-	// DisableStacktrace completely disables automatic stacktrace capturing. By
-	// default, stacktraces are captured for WarnLevel and above logs in
-	// development and ErrorLevel and above in production.
+	Level             string
+	Development       bool
+	DisableCaller     bool
 	DisableStacktrace bool
-	// Encoding sets the logger's encoding. Valid values are "json" and
-	// "console", as well as any third-party encodings registered via
-	// RegisterEncoder.
-	Encoding string
-	// OutputPaths is a list of URLs or file paths to write logging output to.
-	// See Open for details.
-	OutputPaths []string
-	// ErrorOutputPaths is a list of URLs to write internal logger errors to.
-	// The default is standard error.
-	//
-	// Note that this setting only affects internal errors; for sample code that
-	// sends error-level logs to a different location from info- and debug-level
-	// logs, see the package-level AdvancedConfiguration example.
-	ErrorOutputPaths []string
+	Encoding          string
+	OutputPaths       []string
+	ErrorOutputPaths  []string
 }
