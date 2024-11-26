4b863893c4f71fdfced167e9dfdc329e39d6f0ce
Mirza Hilmi <dev@mrz.my.id>
2024-11-26 09:34:06 +0700

    refactor: remove config on json

--- user-service/internal/logging/logging.go
+++ user-service/internal/logging/logging.go
@@ -1,23 +1,13 @@
 package logging
 
 import (
-	"github.com/Lab-ICN/backend/user-service/internal/config"
 	"go.uber.org/zap"
 )
 
-func New(cfg *config.Config) (*zap.Logger, error) {
-	level, err := zap.ParseAtomicLevel(cfg.Logging.Level)
-	if err != nil {
-		return nil, err
-	}
-	_cfg := zap.Config{
-		Level:             level,
-		Development:       cfg.Logging.Development,
-		DisableCaller:     cfg.Logging.DisableCaller,
-		DisableStacktrace: cfg.Logging.DisableStacktrace,
-		Encoding:          cfg.Logging.Encoding,
-		OutputPaths:       cfg.Logging.OutputPaths,
-		ErrorOutputPaths:  cfg.Logging.ErrorOutputPaths,
-	}
-	return _cfg.Build()
+func NewDevelopment() (*zap.Logger, error) {
+	return zap.NewDevelopment()
+}
+
+func NewProduction() (*zap.Logger, error) {
+	return zap.NewProduction()
 }
